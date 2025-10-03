8f57f9afe8316b9922d32f79bdd23df445c2909d
Bhawnadagar123 <bhawna26101998@gmail.com>
2025-10-03 23:11:57 +0530

    add angular frontend skeleton

--- weightloss-frontend/src/app/pages/cart/cart.spec.ts
+++ weightloss-frontend/src/app/pages/cart/cart.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Blog } from './blog';
+import { Cart } from './cart';
 
-describe('Blog', () => {
-  let component: Blog;
-  let fixture: ComponentFixture<Blog>;
+describe('Cart', () => {
+  let component: Cart;
+  let fixture: ComponentFixture<Cart>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Blog]
+      imports: [Cart]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Blog);
+    fixture = TestBed.createComponent(Cart);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
